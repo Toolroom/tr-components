4a6cf50a7dbf2e50b7f53b47ac51ea4a345e8f6e
Martin Liboswar <m.liboswar@toolroom.at>
2018-05-22 01:47:32 +0200

    Adds starts-with support for IE12

--- addon/helpers/starts-with.js
+++ addon/helpers/starts-with.js
@@ -2,9 +2,15 @@ import { helper } from '@ember/component/helper';
 
 export function startsWith(params/*, hash*/) {
     let str = params[0] || '',
-        containedStr = params[1] || '';
+        containedStr = params[1] || '',
+        base = str.toString();
 
-    return str.toString().startsWith(containedStr);
+    if(!base.startsWith)
+    {
+        return base.indexOf(containedStr) >= 0;
+    }
+
+    return base.startsWith(containedStr);
 }
 
 export default helper(startsWith);
