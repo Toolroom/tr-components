9d572d059475b0da42a7fdfb27aa150a71a18ba4
Martin Liboswar <martin@liboswar.net>
2018-05-28 12:54:11 +0200

    Adds textarea editor.
    #3220

---
+++ addon/components/tr-textarea-editor.js
@@ -0,0 +1,7 @@
+import TextEditor from './tr-text-editor';
+import layout from '../templates/components/tr-textarea-editor';
+
+export default TextEditor.extend({
+    layout,
+    maxlength: null
+});
